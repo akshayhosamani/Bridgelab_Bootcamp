7bb11f5f760845215156aa4fdd2f58dbe74494a4
akshayhosamani <xoloakshay@gmail.com>
2021-10-21 19:42:50 +0530

    added percentage

--- flipCoin.sh
+++ flipCoin.sh
@@ -1,8 +1,10 @@
 head=0
 tail=0
+flipTime=0
 #for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21
 for (( i=0;i<21; i++))
 do
+	flipTime=$((flipTime+1))
 	flip=$((RANDOM%2))
 	if [ $flip == 1 ]
 	then
@@ -23,3 +25,9 @@ if [ $head -gt $tail ]
 	echo "Tail has won by $diff points"
 fi
 
+percentageOfHead=$((100*head/flipTime))
+percentageOfTail=$((100*tail/flipTime))
+echo "Flip Times ==> "$flipTime
+
+echo "Pecentage of Head is ==> "$percentageOfHead
+echo "Pecentage of Tail is ==> "$percentageOfTail
